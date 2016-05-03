8421c2ee847a322265efb0d751848f1939407f80
Josh Washke <jwashke@icloud.com>
2016-05-03 16:39:26 -0700

    Implement User model tests and validations

---
+++ test/models/user_test.rb
@@ -0,0 +1,62 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Example Name", email: "email@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    refute @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    refute @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    refute @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    refute @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                     first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                         foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email validates uniqueness" do
+    dup_user = @user.dup
+    @user.save
+    refute dup_user.valid?
+  end
+
+  test "email uniqueness validation is case insensitive" do
+    dup_user = @user.dup
+    @user.save
+    dup_user.email = dup_user.email.upcase
+    refute dup_user.valid?
+  end
+end
